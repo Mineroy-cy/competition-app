664f0328cdd5b7b456b3e4c3b120726a9072bd62
Mineroy-cy <royvictorthuku@gmail.com>
2026-03-26 07:30:51 +0300

    render.yaml

--- server/index.js
+++ server/index.js
@@ -72,4 +72,6 @@ app.use((err, req, res, next) => {
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server started on port ${port}`);
+});
